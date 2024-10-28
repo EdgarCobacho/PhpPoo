30dada80eff9fbd89ed5a68cb324f138acfef145
EdgarCobacho <edgar12ce@gmail.com>
2024-10-28 13:42:23 +0100

    Create README_Ex4.php

---
+++ README_Ex4.php
@@ -0,0 +1,7 @@
+# Php Poo 
+- Ejercicio 4 
+- En este ejercicio me funciona todo.
+- Cree un trait llamado "Loggable" que tenga un método llamado "log" que reciba un
+mensaje y lo imprima con una marca de tiempo. A continuación, cree una clase
+"Producto" que use este trait y registre un mensaje cada vez que se modifique el precio
+del producto.
